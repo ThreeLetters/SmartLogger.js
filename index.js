7226f5e2b29f8ce1b005e42cd76ecf8fc03481a6
Andrew S <andrews54757@gmail.com>
2017-06-04 00:51:18 -0400

    Update

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 var fs = require('fs');
 var styleme = require('styleme');
-styleme.extend();
+class logger {
+    constructor(config) {
 
-console.log = function(/**/) {
-for (var i in arguments) {
-  process.stdout.write(arguments[i] + "\n")
+        this.lvl = config.level || 0;
+        this.presets = config.presets || {};
+
+        this.init();
+    }
+
+
+    log( /**/ ) {
+        arguments = Array.prototype.slice.call(arguments);
+        var i = 0;
+        var options = this.presets;
+        if (typeof arguments[0] == "object") {
+            i = 1;
+            var opt = arguments[0];
+            for (var i in opt) options[i] = opt[i] || options[i];
+        }
+
+        for (; i < arguments.length; i++) {
+            var text = arguments[i],
+                head = options.head || "";
+
+            if (options.style) text = text.styleMe();
+
+            console.log(head + text);
+
+            if (!options.log && (!options.logCat || options.logCat <= this.lvl)) this._log(text, head, options)
+        }
+    }
+    debug( /**/ ) {
+
+
+    }
+    _log(txt, head, options) {
+
+
+
+
+
+    }
+    tracer() {
+
+        var error = new Error();
+        var stack = error.stack;
+        if (!stack) return false;
+
+        stack = stac.split("\n")
+    }
 }
-  
-}
-
-console.debug = function(/**/) {
-
-
-}
-
-function log(txt,head,type) {
-  
-  
-}
-
