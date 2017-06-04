893bff12eae93df5ee0df6f4aa513dbe144aa468
Andrew S <andrews54757@gmail.com>
2017-06-03 23:55:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+var fs = require('fs');
+var styleme = require('styleme');
+styleme.extend();
 
 console.log = function(/**/) {
-
-
+for (var i in arguments) {
+  process.stdout.write(arguments[i] + "\n")
+}
+  
 }
 
 console.debug = function(/**/) {
 
 
 }
+
+function log(txt,head,type) {
+  
+  
+}
+
